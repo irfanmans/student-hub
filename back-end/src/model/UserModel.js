7f79f789f71bca35df2657cc0a447992014c2e65
Irfan Mulya <irfanmulya619@gmail.com>
2025-11-09 12:07:59 +0700

    memperbaiki model database

--- back-end/src/model/UserModel.js
+++ back-end/src/model/UserModel.js
@@ -1,24 +1,23 @@
 import { Sequelize } from "sequelize";
-import db from"../config/Database.js";
+import db from "../config/Database.js";
 
 const { DataTypes } = Sequelize;
 
 const User = db.define(
-    "users",
-    {
-        nim: DataTypes.STRING,
-        nama: DataTypes.STRING,
-        jurusan: DataTypes.STRING,
-        kelas: DataTypes.STRING,
-        email: DataTypes.STRING,
-    },
-    {
-        freezeTableName: true,
-    }
+  "users",
+  {
+    nim: DataTypes.STRING,
+    nama: DataTypes.STRING,
+    jurusan: DataTypes.STRING,
+    email: DataTypes.STRING,
+  },
+  {
+    freezeTableName: true,
+  }
 );
 
 export default User;
 
 (async () => {
-    await db.sync();
+  await db.sync();
 })();
