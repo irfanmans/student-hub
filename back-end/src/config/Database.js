bfb6c3b17e2b2d33f1eb20bdd2e72c06fe20c533
Irfan Mulya <irfanmulya619@gmail.com>
2025-11-09 20:55:46 +0700

    sedikit memperbaiki file Dockerfile di back-end

--- back-end/src/config/Database.js
+++ back-end/src/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("crud_studenthub", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.DB_NAME || "crud_studenthub",
+  process.env.DB_USER || "root",
+  process.env.DB_PASS || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+  }
+);
 
 export default db;
